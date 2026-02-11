ff3b397912cffc6e3c663111b7c9835de909e5c2
Hugo Famania <hugo_24@alu.uabcs.mx>
2026-02-11 10:01:53 -0700

    Se crean las advertencias de inicio de sesion

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		Ventana ventanita = new Ventana();
+
 	}
+	
 
 }
 
--- src/views/Panel.java
+++ src/views/Panel.java
@@ -20,7 +20,7 @@ public class Panel extends JPanel{
 		//setBackground(Color.BLUE);
 		
 		//Esta opción genera un color a partir de RGB
-		setBackground(new Color(210,165,35));
+		setBackground(new Color(250, 128, 114));
 		
 		//se crea la etiqueta de bienvenida
 		
@@ -28,17 +28,40 @@ public class Panel extends JPanel{
 		bienvenida.setFont(new Font("Arial",Font.PLAIN,30));
 		bienvenida.setBounds(160,0,250,100);
 		add(bienvenida);
-		//se crea el boton
 		
+		//lbl de ingresar correo
 		JLabel correo = new JLabel("Ingresa tu correo");
 		correo.setFont(new Font("Arial",Font.PLAIN,20));
 		correo.setBounds(160,175,200,50);
 		add(correo);
 		
+		//txt ingresar correo
 		JTextField txtCorreo = new JTextField();
-		txtCorreo.setFont(new Font("Arial",Font.PLAIN,20) );	
+		txtCorreo.setFont(new Font("Arial",Font.PLAIN,10) );	
 		txtCorreo.setBounds(160,210,200 ,25);
 		add(txtCorreo);
+		
+		
+		//label de inicio de sesion incorrecto
+		JLabel lblError = new JLabel("Contraseña o correo incorrecto");
+		lblError.setFont(new Font("Arial",Font.PLAIN,12) );	
+		lblError.setBounds(160,290,170 ,50);	
+		lblError.setForeground(Color.red);
+		add(lblError);
+		
+		//lbl de ingresar correo
+		JLabel lblErrorCorreo = new JLabel("!");
+		lblErrorCorreo.setFont(new Font("Arial",Font.PLAIN,20) );	
+		lblErrorCorreo.setBounds(150,195,50 ,50);	
+		lblErrorCorreo.setForeground(Color.red);
+		add(lblErrorCorreo);
+		
+		//lbl de ingresar contraseña
+		JLabel lblErrorPass = new JLabel("!");
+		lblErrorPass.setFont(new Font("Arial",Font.PLAIN,20) );	
+		lblErrorPass.setBounds(150,240,50 ,50);	
+		lblErrorPass.setForeground(Color.red);
+		add(lblErrorPass);
 		
 		JLabel contrasena = new JLabel("Ingresa tu Contrasena");
 		contrasena.setFont(new Font("Arial",Font.PLAIN,20));
